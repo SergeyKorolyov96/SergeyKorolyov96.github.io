cf8907ee9bb23b9d8eb4e4e69ee085df3aa45495
SergeyKorolyov96 <87485461+SergeyKorolyov96@users.noreply.github.com>
2021-09-02 01:14:06 +0600

    JavaScript

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,1 +1,13 @@
-console.log("Привет, мир");
+"use strict"
+function calc(){
+    let type_services = document.getElementById("type_services");
+    let design = document.getElementById("design");
+    let adapt = document.getElementById("adapt");
+    let result = document.getElementById("result");
+    let price = 0;
+    price += parseInt(type_services.options[type_services.selectedIndex].value);
+    price += (design.checked == true) ? parseInt(design.value) : 0;
+    price = parseInt(adapt.value) * price;
+    result.innerHTML = price;
+}
+console.log('calc');
